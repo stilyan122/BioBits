339b1401f7997abd79f226219f52c100ffa33e5a
Stilyan <117260079+stilyan122@users.noreply.github.com>
2025-10-28 14:27:18 +0200

    chore: Initial commit (Expo navigation TS template)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+// Imports which load the vector icon, font metadata so icons render correctly.
+
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
@@ -5,33 +7,34 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// This tells Expo not to hide the splash automactically - we'll hide it manually
+// once the fonts are loaded.
 SplashScreen.preventAutoHideAsync();
 
+// Waits for fonts. 
 export default function RootLayout() {
+  // Asynchronously loads custom fonts.
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  // If there was an error loading the fonts, throw it.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
 
+  // When loaded becomes true, it hides the splash screen.
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -42,12 +45,17 @@ export default function RootLayout() {
     return null;
   }
 
+  // Renders the root layout navigation.
   return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
+  // This helper returns the current color scheme (light or dark or system).
   const colorScheme = useColorScheme();
 
+  // We pass the scheme to the ThemeProvider to get dark/light colors for headers, backgrounds, etc.
+  
+  // Stack is from the expo-router and defines a stack navigator at the root.
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
